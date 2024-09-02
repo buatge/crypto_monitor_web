b4904e4e61626b3a7b18f11012f39702e32509b9
Bourbon.l <bu__ge@hotmail.com>
2024-09-02 10:59:52 +0800

    [feat][0.02][monitor]: 增加用户的symbol watchlist展示和操作

--- src/common/api/request.js
+++ src/common/api/request.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const request = axios.create({
     baseURL: 'http://192.168.1.18:5001/', // 修改为你实际的 API 基础路径
+    // baseURL:'http://127.0.0.1:5001',
     timeout: 20000,
     withCredentials: true, // 确保请求时带上凭证（如 Cookie
 });
